fed74e865bc1d9c3c442eb5b07adcefa8f09ef57
epodivilov <evgeniy.podivilov@gmail.com>
2015-10-29 14:37:35 +0500

    Created minNotContains function. The function takes a single parameter - an array of valid JavaScript numbers (both positive and negative) The function returns the smallest positive integer, not represented in the array.

---
+++ Task1.js
@@ -0,0 +1,14 @@
+function minNotContains(array) {
+    var sortedArray = array.sort(function (a, b) { return a-b });
+    var minElement = 1;
+    for (var i = 0; i < sortedArray.length; i++ ) {
+        if (sortedArray[i] > 0) {
+            if (sortedArray[i] > minElement) {
+                return minElement;
+            } else {
+                minElement++;
+            }
+        }
+    }
+    return minElement;
+}
