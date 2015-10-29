9cc9f21ac916d9069a28e212c124220d1c1c4c6b
epodivilov <evgeniy.podivilov@gmail.com>
2015-10-29 16:23:34 +0500

    Added function isBktBalanced. The function takes as argument the string. If the string is located right parenthesis, the line is considered to be balanced, and the function returns true. Otherwise, it returns false.

---
+++ Task2.js
@@ -0,0 +1,33 @@
+function isBktBalanced (string) {
+    var brackets = "[]{}()";
+    var stack = [];
+    var i = 0;
+    var symbol;
+
+    while (symbol = string[i++])
+    {
+        var bracePosition = brackets.indexOf(symbol);
+
+        if (bracePosition == -1) {
+            continue;
+        }
+
+        var braceType = bracePosition % 2 ? 'closed' : 'open';
+
+        if (braceType === 'closed') {
+            if (!stack.length || brackets.indexOf(stack.pop()) != bracePosition - 1) {
+                return false;
+            }
+        } else {
+            stack.push(symbol);
+        }
+    }
+    return !Boolean(stack.length);
+}
+
+console.log(' "" = ' + isBktBalanced('""'));
+//console.log(' [] = ' + isBktBalanced('[]'));
+//console.log(' [()] = ' + isBktBalanced('[()]'));
+//console.log(' [](){} = ' + isBktBalanced('[](){}'));
+//console.log(' ][ = ' + isBktBalanced(']['));
+//console.log(' [{]} = ' + isBktBalanced('[{]}'));
