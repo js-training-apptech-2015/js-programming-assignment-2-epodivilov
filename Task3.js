3f3d97863e4eddbf6359350ec52a18e29fec1dd8
epodivilov <evgeniy.podivilov@gmail.com>
2015-10-29 20:05:05 +0500

    Create two functions: Function parseFunc takes as argument the string containing the arrow function and returns a new function. Function solution3 takes an array and the string containing  the arrow function and returns an array after applying to it the function of the second argument.

--- Task3.js
+++ Task3.js
@@ -1,1 +1,38 @@
+function solution3 (array, func) {
+    var mapedFunc = parseFunc(func);
+    for (var i = 0; i < array.length; i++) {
+        array[i] = mapedFunc(array[i]);
+    }
 
+    return array;
+}
+
+function parseFunc(string) {
+    var regArgs = /\((.*)\)/;
+    var regBody = />(.*)/;
+    var arguments;
+    var body;
+
+    if (regArgs.test(string)) {
+        arguments = regArgs.exec(string)[1];
+        if (/,/.test(arguments)) {
+            arguments = arguments.split(',');
+        }
+    } else {
+        arguments = [];
+    }
+
+    if (regBody.test(string)) {
+        body = "return " + regBody.exec(string)[1] + ";";
+    } else {
+        body = "return;"
+    }
+
+    return new Function(arguments, body);
+}
+
+
+
+//console.log("solution3([1,2,3], '(a)=>a*2') | " + solution3([1,2,3], '(a)=>a*2'));
+//console.log("solution3([1,2,3], '(a)=>a+2') | " + solution3([1,2,3], '(a)=>a+2'));
+//console.log("solution3([1,2,3], '()=>1') | " + solution3([1,2,3], '()=>1'));
